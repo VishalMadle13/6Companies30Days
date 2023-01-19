ae5449e16049ddff54b7b408e8ea951af51f3720
vishal <vishalmadle13@gmail.com>
2023-01-19 15:55:16 +0530

    commit

--- day02/MS_05.cpp
+++ day02/MS_05.cpp
@@ -30,3 +30,44 @@ Output: [1,2,4,8]
 
  
 */
+
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int max = 1;
+        vector<int> dp(n+1,1);
+        for(int i=1;i<n;++i)
+        {
+            for(int j=0;j<i;++j)
+            {
+                if(nums[i]%nums[j]==0 && 1+dp[j]>dp[i])
+                {
+                    dp[i] = 1+dp[j];
+                    if(max<dp[i])
+                        max = dp[i];
+                }
+            }
+        }
+        for(auto x : dp){
+            cout<<x<<" ";
+        }
+
+        int prev = -1;
+         for(int i=n-1;i>=0;i--)
+        {
+            if(dp[i]==max &&  (prev%nums[i]==0 || prev==-1))
+            {
+                ans.push_back(nums[i]);
+                max -=1;
+                prev = nums[i];
+            }
+        }
+        return ans;
+
+     
+    }
+};
